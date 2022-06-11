e1923a86a6061ce98ffcdb92b0db02e04be73b5b
Ihor Hrebeniuk <sabor7dex@gmail.com>
2022-06-11 19:53:56 +0300

    main

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const maxImg = document.querySelector('.right-panel img');
 const select = document.querySelector('select');
 const allLang = ['en', 'ru', 'ua'];
 
-document.querySelectorAll('.left-panel img').forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
+//document.querySelectorAll('.left-panel img').forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
 
 select.addEventListener('change', changeURLLanguage);
 
--- js/lang.js
+++ js/lang.js
@@ -1,32 +1,146 @@
 const langArr = {
+    //Главная страница
     "unit" :  {
-        "ru": "создаем многоязычный сайт",
-        "en": "build a multi language site ",
-        "ua": "створюємо багатомовний сайт",
-    }, 
-    "chip": {
-        "ru": "графический чип",
-        "en": "graphics processing unit",
-        "ua": "графічний чіп",
-    }, 
-    "memory": {
-        "ru": "объем памяти",
-        "en": "memory size",
-        "ua": "oбсяг пам'яті",
-    }, 
-    "memory-type": {
-        "ru": "тип памяти",
-        "en": "memory type",
-        "ua": "nип пам'яті",
-    }, 
-    "cool": {
-        "ru": "тип системы охлаждения",
-        "en": "сooling system type",
-        "ua": "тип системи охолодження",
-    }, 
-    "more": {
-        "ru": "детально о товаре",
-        "en": "more details",
-        "ua": "детально про товар",
-    }, 
+        "ru": "Игорь Гребенюк",
+        "en": "Ihor Hrebeniuk",
+        "ua": "Iгор Гребенюк",
+    },
+    "speciality": {
+        "ru": "фрилансер, фронтенд-разработчик, веб-разработчик, разработка сайтов",
+        "en": "freelancer, front-end developer, web developer, website development",
+        "ua": "фрілансер, front-end розробник, веб-розробник, розробка веб-сайтів",
+    },
+    "home-portfolio": {
+        "ru": "Портфолио",
+        "en": "Portfolio",
+        "ua": "Портфолiо",
+    },
+    //карточка 1 на главной странице
+    "card1-title": {
+        "ru": "Целевая страница",
+        "en": "Landing page",
+        "ua": "Цільова сторінка",
+    },
+    "card1-text": {
+        "ru": "Целевая страница на HTML/CSS/JavaScript",
+        "en": "Landing page на HTML/CSS/JavaScript",
+        "ua": "Цільова сторінка на HTML/CSS/JavaScript",
+    },
+    //карточка 2 на главной странице
+    "card2-title": {
+        "ru": "Целевая страница",
+        "en": "Landing page",
+        "ua": "Цільова сторінка",
+    },
+    "card2-text": {
+        "ru": "Целевая страница на HTML/CSS/JavaScript",
+        "en": "Landing page на HTML/CSS/JavaScript",
+        "ua": "Цільова сторінка на HTML/CSS/JavaScript",
+    },
+    //карточка 3 на главной странице
+    "card3-title": {
+        "ru": "Сайт портфолио",
+        "en": "Portfolio site",
+        "ua": "Сайт портфолiо",
+    },
+    "card3-text": {
+        "ru": "Сайт портфолио на HTML/CSS/JavaScript",
+        "en": "Portfolio website in HTML/CSS/JavaScript",
+        "ua": "Сайт портфоiо на HTML/CSS/JavaScript",
+    },
+    //карточка 4 на главной странице
+    "card4-title": {
+        "ru": "Многостраничный блог-сайт ",
+        "en": "Multi-page blog site",
+        "ua": "Багатосторінковий блог-сайт",
+    },
+    "card4-text": {
+        "ru": "Многостраничный блог-сайт на HTML/CSS/JavaScript",
+        "en": "Layout of a multi-page blog site in HTML/CSS/JavaScript",
+        "ua": "Багатосторiнковий блог-сайт на HTML/CSS/JavaScript",
+    },
+    //подвал
+    "contact-telegram": {
+        "ru": "Контакт Telegram",
+        "en": "Contact Telegram",
+        "ua": "Контакт Telegram",
+    },
+    "contact-telegram-text": {
+        "ru": "Напишите мне для заказа или узнайте стоимость вашего проекта",
+        "en": "Write to me to order or find out the cost of layout of your project",
+        "ua": "Напишіть мені для замовлення або дізнайтеся вартість макета вашого проекту",
+    },
+    "icon-text": {
+        "ru": "Мои профили в социальных сетях",
+        "en": "My social media profiles",
+        "ua": "Мої профілі в соціальних мережах",
+    },
+    "copyright-name": {
+        "ru": "Игорь Гребенюк",
+        "en": "Ihor Hrebeniuk",
+        "ua": "Iгор Гребенюк",
+    },
+    "copyright-text": {
+        "ru": "HTML верстка и разработка сайта",
+        "en": "HTML layout and website development",
+        "ua": "HTML верстка та розробка сайту",
+    },
+
+    //Сайты
+
+    "go-link": {//
+        "ru": "Ссылка сайта",
+        "en": "Website address",
+        "ua": "Посилання",
+    },
+    "go-back": {
+        "ru": "Вернуться на главную страницу",
+        "en": "Go back to the main page",
+        "ua": "Повернутися на головну сторінку",
+    },
+         //Название и описание каждого сайта
+    //ActiveBox
+    "names": {
+        "ru": "Верстка лендинга ActiveBox",
+        "en": "Landing layout ActiveBox",
+        "ua": "Верстка лендинга ActiveBox",
+    },
+    "description": {
+        "ru": "Верстка лендинга на HTML/CSS",
+        "en": "Landing layout in HTML/CSS",
+        "ua": "Верстка лендинга на HTML/CSS",
+    },
+    //Parcel
+    "names-parcel": {
+        "ru": "Верстка лендинга Parcel",
+        "en": "Landing layout Parcel",
+        "ua": "Верстка лендинга Parcel",
+    },
+    "description-parcel": {
+        "ru": "Верстка лендинга на HTML/CSS",
+        "en": "Landing layout in HTML/CSS",
+        "ua": "Верстка лендинга на HTML/CSS",
+    },
+     //MyPortfolio
+     "names-myportfolio": {
+        "ru": "Верстка сайта My portfolio",
+        "en": "Layout of the site My portfolio",
+        "ua": "Верстка сайта My portfolio",
+    },
+    "description-myportfolio": {
+        "ru": "Верстка многостраничного сайта на HTML/CSS/JavaScript . Видео размещено на главной странице с помощью библиотеки jQuery",
+        "en": "Layout of a multi-page site in HTML/CSS/JavaScript. The video is placed on the main page using the jQuery library",
+        "ua": "Верстка багатосторiнкового сайта на HTML/CSS/JavaScript . Видео размiщене на головнiй сторiнкi за допомогою бiблiотеки jQuery",
+    },
+    //PersonalBlog
+    "names-personalblog": {
+        "ru": "Верстка сайта блога PersonalBlog",
+        "en": "Website layout PersonalBlog",
+        "ua": "Верстка сайта блога PersonalBlog",
+    },
+    "description-personalblog": {
+        "ru": "Вёрстка многостраничного блога на HTML/CSS/JavaScript. Задействованы препроцессор SASS и сборщик GULP. Сайт имеет три модальных окна",
+        "en": "Layout of a multi-page blog site in HTML/CSS/JavaScript. The SASS preprocessor and the GULP collector are involved. The site has three modal windows",
+        "ua": "Верстка багатосторiнкового сайта блога на HTML/CSS/JavaScript. Задіяні препроцесор SASS та збирач GULP. Сайт має три модальні вікна ",
+    },
 }
